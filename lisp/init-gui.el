9d5c1230fb0633130d72ec54ce97c7d703a76845
maowu122 <maowu122@outlook.com>
2018-07-05 01:54:05 +0800

    独立GUI配置

--- lisp/init-gui.el
+++ lisp/init-gui.el
@@ -1,14 +1,5 @@
 ;; Interface
 
-;; theme
-(load-theme 'gruvbox t)
-(setq evil-emacs-state-cursor '("red" box))
-(setq evil-normal-state-cursor '("green" box))
-(setq evil-visual-state-cursor '("orange" box))
-(setq evil-insert-state-cursor '("red" bar))
-(setq evil-replace-state-cursor '("red" bar))
-(setq evil-operator-state-cursor '("red" hollow))
-
 (defun set-font(en-font zh-font size)
   (interactive)
   (set-face-attribute 'default nil :family en-font)
@@ -31,23 +22,11 @@
 (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
 (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease)
 
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(global-linum-mode t)
-
-
 (setq inhibit-splash-screen 1)
 ;; 默认全屏
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 ;; 在标题栏显示buffer的名字
 (setq frame-title-format "%b@emacs")
 (setq linum-format "%4d ")
-
-;;显示匹配括号
-(show-paren-mode t)
-(setq show-paren-style 'parentheses)
-;; 高亮当前行
-(global-hl-line-mode 1)
 
 (provide 'init-ui)
